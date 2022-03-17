9ec16f3e4c97f73df3f8ead80658aa5b7b4ed367
jd <jd@LAB220-17>
2022-03-17 13:51:26 +0100

    reading file and comparer

--- APBD2/APBD2/Program.cs
+++ APBD2/APBD2/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using APBD2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace APBD2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = args[0];
+
+            //var fi = new FileInfo(path);
+            FileInfo fi = new(path);
+
+            var fileContent = new List<string>();
+
+            using (StreamReader stream = new(fi.OpenRead()))
+            {
+                string line = null;
+
+                while ((line = await stream.ReadLineAsync()) != null)
+                {
+                    fileContent.Add(line);
+                }
+
+                //stream.Dispose();
+            }
+
+            /*
+             * foreach (var item in File.ReadLines(path)){} 
+             */
+
+            foreach (var item in fileContent)
+            {
+                Console.WriteLine(item);
+            }
+
+            //DateTime
+
+            var hashSet = new HashSet<Student>(new MyComparer());
+
         }
     }
 }
